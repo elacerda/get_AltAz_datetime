81f5d82168ddcc15d485ac3bd2a276f2b9a1853e
Eduardo Alberto Duarte Lacerda <dhubax@gmail.com>
2023-07-17 12:22:26 -0300

    script bash date loop

--- scripts/get_AltAz_datetime.sh
+++ scripts/get_AltAz_datetime.sh
@@ -1,4 +1,9 @@
-DT=$1
-O_FILE=${DT}_lonlat_datetime.csv
+d=$1
+DT=$2
 
-python3 ../get_AltAz_datetime.py -d $DT >> $O_FILE
+while [ "$d" != "$DT" ]
+do
+    O_FILE=${d}_lonlat_datetime.csv
+    python3 ../get_AltAz_datetime.py -d $d >> $O_FILE
+    d=$(TZ=UTC date "+%Y%m%d" -d "$d + 1 day")
+done
